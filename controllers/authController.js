d31cf83e22bd7bf08e559c85a5584e76c7c92a11
Qasimsaifi <kasimytcreation@gmail.om>
2025-03-12 23:45:02 +0530

    addded

--- controllers/authController.js
+++ controllers/authController.js
@@ -106,7 +106,7 @@ exports.googleAuth = (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 3600000 * 24 * 100,
     path: "/",
   });
